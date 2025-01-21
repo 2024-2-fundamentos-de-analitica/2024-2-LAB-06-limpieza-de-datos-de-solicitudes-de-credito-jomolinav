bc41d7057ae488c16856b85cca1ebe327099caf0
jomolinav <jomolinav@unal.edu.co>
2025-01-21 14:50:52 -0500

    Labupd4

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -1,15 +1,37 @@
-"""
-Escriba el codigo que ejecute la accion solicitada en la pregunta.
-"""
+
 
 
 def pregunta_01():
-    """
-    Realice la limpieza del archivo "files/input/solicitudes_de_credito.csv".
-    El archivo tiene problemas como registros duplicados y datos faltantes.
-    Tenga en cuenta todas las verificaciones discutidas en clase para
-    realizar la limpieza de los datos.
 
-    El archivo limpio debe escribirse en "files/output/solicitudes_de_credito.csv"
+    import pandas as pd
+    
+    df = pd.read_csv("files/input/solicitudes_de_credito.csv", sep=";", index_col=0)
+    df = df.copy()
 
-    """
+    columnas = ["sexo", "tipo_de_emprendimiento", "idea_negocio", "monto_del_credito", "línea_credito"]
+    for col in columnas:
+        if col in df.columns:
+            df[col] = df[col].str.lower().str.strip()
+            df[col] = df[col].str.replace("_", " ").str.replace("-", " ")
+            df[col] = df[col].str.replace(",", "").str.replace("$", "")
+            df[col] = df[col].str.replace(".00", "").str.strip()
+
+    if "barrio" in df.columns:
+        df["barrio"] = df["barrio"].str.lower()
+        df["barrio"] = df["barrio"].str.replace("_", " ").str.replace("-", " ")
+
+    if "comuna_ciudadano" in df.columns:
+        df["comuna_ciudadano"] = pd.to_numeric(df["comuna_ciudadano"], errors="coerce", downcast="integer")
+    if "monto_del_credito" in df.columns:
+        df["monto_del_credito"] = pd.to_numeric(df["monto_del_credito"], errors="coerce")
+    if "fecha_de_beneficio" in df.columns:
+        df["fecha_de_beneficio"] = pd.to_datetime(
+            df["fecha_de_beneficio"], format="%d/%m/%Y", errors="coerce"
+        ).combine_first(
+            pd.to_datetime(df["fecha_de_beneficio"], format="%Y/%m/%d", errors="coerce")
+        )
+
+    df = df.drop_duplicates()
+    df = df.dropna()
+    output_path = "files/output/solicitudes_de_credito.csv"
+    df.to_csv(output_path, sep=";",index=False)
